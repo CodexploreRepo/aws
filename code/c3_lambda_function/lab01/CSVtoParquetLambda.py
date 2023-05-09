549f2ff39d3888ca6d2229eea204e337bf458ec7
CodeXploreRePo <codexplore.channel@gmail.com>
2023-05-09 23:46:44 +0800

    update git ignore

--- code/c3_lambda_function/lab01/CSVtoParquetLambda.py
+++ code/c3_lambda_function/lab01/CSVtoParquetLambda.py
@@ -1,31 +1,38 @@
 from urllib.parse import unquote_plus
 
 import awswrangler as wr
-import boto3
 
 
 def lambda_handler(event, context):
     # Get the source bucket and object name as passed to the Lambda function
     for record in event["Records"]:
         bucket = record["s3"]["bucket"]["name"]
+        # unquote: replace %xx escapes with their single-character equivalent.
+        #  -> unquote('/El%20Ni%C3%B1o/') yields '/El Niño/'
+        # unquote_plus: replace plus signs with spaces, as required for unquoting HTML form values.
+        # -> unquote_plus('/El+Ni%C3%B1o/') yields '/El Niño/'
         key = unquote_plus(record["s3"]["object"]["key"])
 
     # We will set the DB and table name based on the last two elements of
     # the path prior to the file name. If key = 'dms/sakila/film/LOAD01.csv',
     # then the following lines will set db to sakila and table_name to 'film'
     key_list = key.split("/")
+    # key_list: ['testdb', 'csvparquet', 'test.csv']
     print(f"key_list: {key_list}")
     db_name = key_list[len(key_list) - 3]
     table_name = key_list[len(key_list) - 2]
 
-    print(f"Bucket: {bucket}")
-    print(f"Key: {key}")
-    print(f"DB Name: {db_name}")
-    print(f"Table Name: {table_name}")
+    print(f"Bucket: {bucket}")  # Bucket: upskills-landing-zone
+    print(f"Key: {key}")  # Key: testdb/csvparquet/test.csv
+    print(f"DB Name: {db_name}")  # DB Name: testdb
+    print(f"Table Name: {table_name}")  # Table Name: csvparquet
 
     input_path = f"s3://{bucket}/{key}"
+    # Input_Path: s3://upskills-landing-zone/testdb/csvparquet/test.csv
     print(f"Input_Path: {input_path}")
+
     output_path = f"s3://upskills-clean-zone/{db_name}/{table_name}"
+    # Output_Path: s3://upskills-clean-zone/testdb/csvparquet
     print(f"Output_Path: {output_path}")
 
     input_df = wr.s3.read_csv([input_path])
@@ -40,10 +47,10 @@ def lambda_handler(event, context):
     try:
         result = wr.s3.to_parquet(
             df=input_df,
-            path=output_path,
-            dataset=True,
-            database=db_name,
-            table=table_name,
+            path=output_path,  # s3://upskills-clean-zone/testdb/csvparquet
+            dataset=True,  # There is no need .parquet in the output path
+            database=db_name,  # Glue/Athena catalog
+            table=table_name,  # Glue/Athena catalog
             mode="append",
         )
     except Exception as e:
